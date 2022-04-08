06abde5afc8a43c950171cbe514a27c674718bfe
ShaRaya <sharayascovil@u.northwestern.edu>
2022-04-07 19:15:35 -0600

    Initial commit

---
+++ src/main/java/com/tts/Deployment/MainController.java
@@ -0,0 +1,13 @@
+package com.tts.Deployment;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping(value ="/")
+    public String home(Model model) {
+        return "index";
+    }
+}
